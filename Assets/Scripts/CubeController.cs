f1731e98633650ea7620e033fae24685276e5b27
Kenneth <kenships0402@gmail.com>
2026-03-16 16:27:51 -0400

    rotates nicely

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -1,4 +1,5 @@
 using System;
+using PrimeTween;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,7 +8,8 @@ public class CubeController : MonoBehaviour
     [SerializeField] private InputReaderSO inputReader;
     [SerializeField] private Transform pivot;
     private Collider[] colliders = new Collider[9];
-
+    private bool _isRotating = false;
+    
     private void Start()
     {
         inputReader.OnLeftClick += OnLeftClick;
@@ -15,21 +17,33 @@ public class CubeController : MonoBehaviour
 
     private void OnLeftClick()
     {
+        if (_isRotating) return;
+        
         Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
         if (!Physics.Raycast(ray, out RaycastHit hit))
         {
             return;
-        }
+        } 
+        _isRotating = true;
         Quaternion boxRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
         Physics.OverlapBoxNonAlloc(hit.normal, new Vector3(3f, 0.4f, 3f), colliders, boxRotation);
         foreach (var col in colliders)
         {
             col.transform.SetParent(pivot);
-        } 
-        pivot.Rotate(hit.normal, 90, Space.World);
-        foreach (var col in colliders)
-        {
-            col.transform.SetParent(transform);
         }
+    
+        Quaternion targetRotation = Quaternion.AngleAxis(90, hit.normal)*pivot.rotation;
+        Tween.Rotation(
+            target: pivot,
+            endValue: targetRotation,
+            duration: 0.2f
+        ).OnComplete(() =>
+                     {
+                         foreach (var col in colliders)
+                         {
+                             col.transform.SetParent(transform);
+                         }
+                         _isRotating = false;
+                     });
     }
 }
